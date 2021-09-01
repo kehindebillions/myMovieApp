c7ef2a488b758eca1e57bba961699cce74aa0b51
kehinde ogunyade <ogunyade.kehinde@gmail.com>
2021-09-01 14:11:32 +0100

    first commit

---
+++ src/components/MovieList.js
@@ -0,0 +1,15 @@
+import React, { Fragment } from 'react';
+
+const MovieList = (props) => {
+    return (
+        <>
+            {props.movies.map((movie, index) => ( 
+            <div className='d-flex justify-content-start m-1'>
+                <img src={movie.Poster} alt='movie'></img>
+            </div>
+            ))}
+        </>
+    );
+};
+
+export default MovieList;
